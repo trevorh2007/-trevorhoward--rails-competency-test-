9df0d8c74071258b2de25f1ad9dc5ee20ca92664
Trevor <trevorhoward2@hotmail.com>
2017-09-16 14:12:52 -0600

    Add test for registration, need views for routes

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 feature 'User registration' do
+  scenario 'user enters valid input' do
+    visit '/register/join'
 
+    within '#new_user' do
+      fill_in 'Email',    with: 'signup@test.com'
+      fill_in 'Password', with: 'asdfasdf'
+      fill_in 'Password confirmation', with: 'asdfasdf'
+      click_button 'Sign up'
+    end
+
+    page.must_have_content 'Check out the top stories of the day!'
+    page.current_path.must_equal '/'
+  end
 end
