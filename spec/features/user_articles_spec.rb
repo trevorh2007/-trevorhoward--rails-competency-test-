4ddb8098e5151eea5a8d56e5043eb5e30f1044a0
Trevor <trevorhoward2@hotmail.com>
2017-09-16 16:35:36 -0600

    Write failing test for user being able to edit link

--- spec/features/user_articles_spec.rb
+++ spec/features/user_articles_spec.rb
@@ -1,18 +1,24 @@
 require "rails_helper"
 
 describe "User Article" do
+	before do
+		@user = FactoryGirl.create(:user)
+		article = FactoryGirl.create(:article)
+	end
 
 	it "can be viewed on show page by a logged in user" do
-		FactoryGirl.create(:article)
-		user = FactoryGirl.create(:user)
-		login_as(user)
+		login_as(@user)
 		visit '/articles/1'
 
 		expect(page).to have_content("User Article")
 	end
 
 	it "can not be edited" do
+		visit '/articles'
+		click_link('Edit')
 
+		expect(current_path).to eq('/articles')
+		expect(page).to have_content('Permission Denied')
 	end
 
 	it "can not be deleted" do
