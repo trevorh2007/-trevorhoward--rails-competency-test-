f773889a5036e8a7ddc38aaff2a9e3ecc5139e04
Trevor <trevorhoward2@hotmail.com>
2017-09-16 14:39:54 -0600

    Added some seed data to create user, admin, editor, and 3 articles with a different category each

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: "user@test.com", password: "asdfasdf", roles: "user")
+
+puts "1 User created"
+
+User.create!(email: "admin@test.com", password: "asdfasdf", roles: "site_admin")
+
+puts "1 Admin created"
+
+User.create!(email: "editor@test.com", password: "asdfasdf", roles: "editor")
+
+puts "1 Editor created"
+
+3.times do |article|
+	Article.create!(
+		title: "Article #{article}",
+		content: "Bacon ipsum dolor amet shankle prosciutto drumstick, chicken pork loin sirloin strip steak tail ham brisket. Tongue corned beef picanha turkey, porchetta strip steak short loin turducken shankle kielbasa. Flank jerky frankfurter drumstick pancetta. Pastrami cupim pancetta, pork loin kielbasa tenderloin fatback drumstick frankfurter ham biltong. Andouille brisket filet mignon shoulder kielbasa porchetta beef ribs pancetta chuck turducken pork loin pork belly landjaeger prosciutto. Fatback chuck leberkas, biltong alcatra tongue ham hock sausage pork chop tail shankle sirloin.",
+		category: "News",
+		user_id: User.first.email
+		)
+end
+
+3.times do |article|
+	Article.create!(
+		title: "Article #{article + 3}",
+		content: "Bacon ipsum dolor amet shankle prosciutto drumstick, chicken pork loin sirloin strip steak tail ham brisket. Tongue corned beef picanha turkey, porchetta strip steak short loin turducken shankle kielbasa. Flank jerky frankfurter drumstick pancetta. Pastrami cupim pancetta, pork loin kielbasa tenderloin fatback drumstick frankfurter ham biltong. Andouille brisket filet mignon shoulder kielbasa porchetta beef ribs pancetta chuck turducken pork loin pork belly landjaeger prosciutto. Fatback chuck leberkas, biltong alcatra tongue ham hock sausage pork chop tail shankle sirloin.",
+		category: "Space",
+		user_id: User.second.email
+		)
+end
+
+3.times do |article|
+	Article.create!(
+		title: "Article #{article + 6}",
+		content: "Bacon ipsum dolor amet shankle prosciutto drumstick, chicken pork loin sirloin strip steak tail ham brisket. Tongue corned beef picanha turkey, porchetta strip steak short loin turducken shankle kielbasa. Flank jerky frankfurter drumstick pancetta. Pastrami cupim pancetta, pork loin kielbasa tenderloin fatback drumstick frankfurter ham biltong. Andouille brisket filet mignon shoulder kielbasa porchetta beef ribs pancetta chuck turducken pork loin pork belly landjaeger prosciutto. Fatback chuck leberkas, biltong alcatra tongue ham hock sausage pork chop tail shankle sirloin.",
+		category: "Technology",
+		user_id: User.last.email
+		)
+end
+
+puts "9 articles created"
