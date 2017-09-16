765b1a7356e1abd1d8b92ce74fcbe4fbc2599e4d
Trevor <trevorhoward2@hotmail.com>
2017-09-16 17:04:06 -0600

    Finish logout test

--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-describe 'Logout' do
+describe "Logout" do
+
+  it 'can be performed by user' do
+  	user = FactoryGirl.create(:user)
+  	login_as(user)
+  	visit root_path
+  	click_link('Logout')
+
+  	expect(current_path).to eq('/')
+  	expect(page).to have_content("Sign Up")
+  end
+
+  it 'can be performed by editor' do
+  	editor = FactoryGirl.create(:editor)
+  	login_as(editor)
+  	visit root_path
+  	click_link('Logout')
+
+  	expect(current_path).to eq('/')
+  	expect(page).to have_content("Sign Up")
+  end
+
+  it 'can be performed by admin' do
+  	admin = FactoryGirl.create(:admin)
+  	login_as(admin)
+  	visit root_path
+  	click_link('Logout')
+
+  	expect(current_path).to eq('/')
+  	expect(page).to have_content("Sign Up")
+  end    
 
 end
