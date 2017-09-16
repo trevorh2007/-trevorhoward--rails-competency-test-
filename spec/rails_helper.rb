a9a56e5db848cc9b32a47a1b9bc779aa07de4a8d
Trevor <trevorhoward2@hotmail.com>
2017-09-16 12:50:54 -0600

    Add devise test controller helpers for warden

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,4 +22,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
