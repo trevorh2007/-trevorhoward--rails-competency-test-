b547bc68bde9e46bada70dea6d62c55711723523
Trevor <trevorhoward2@hotmail.com>
2017-09-16 16:22:29 -0600

    Add blank test for editor user

---
+++ spec/features/editor_articles_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Editor Article" do
+	before do
+		editor = FactoryGirl.create(:editor)
+	end
+
+	it 'can be created' do
+
+	end
+
+	it 'can be deleted' do
+
+	end
+
+	it 'can be edited' do
+
+	end
+	
+end
