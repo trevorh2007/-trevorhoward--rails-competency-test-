7ef847dc9d564ac5c90312e7dfc5ebf7bb3a4ea3
Trevor <trevorhoward2@hotmail.com>
2017-09-16 12:28:07 -0600

    Generate pages controller and article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'join' }
   
   root to: 'pages#home'
