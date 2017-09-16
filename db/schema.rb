cac28c6c4d40361ac2e1cca1fe9f5a02a8832cbb
Trevor <trevorhoward2@hotmail.com>
2017-09-16 14:22:53 -0600

    Migrate guest user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916185628) do
+ActiveRecord::Schema.define(version: 20170916201645) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20170916185628) do
     t.text "content"
     t.string "category"
     t.integer "user_id"
+  end
+
+  create_table "guest_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
