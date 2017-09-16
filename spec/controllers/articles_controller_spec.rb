e32b7c29ebba07692367d6376ca6b9fc57322caf
Trevor <trevorhoward2@hotmail.com>
2017-09-16 14:11:52 -0600

    Clean up articles controller spec and add user registration test file

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -12,9 +12,6 @@ RSpec.describe ArticlesController, type: :controller do
     { title: nil, category: 1, content: nil, user_id: "42" }
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ArticlesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET #index" do
