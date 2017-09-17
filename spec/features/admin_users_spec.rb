c5858558285959516a46bf6b740fdf7556523982
Trevor <trevorhoward2@hotmail.com>
2017-09-16 19:45:40 -0600

    Add edit and delete tests to admin users

--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -21,9 +21,34 @@ describe 'Admin' do
     expect(current_path).to eq '/admin/user'
     expect(page).to have_content("User successfully created")
     expect(page).to have_content("logintest@example.com")
+    expect(User.last.email).to eq("logintest@example.com")
 	end
 
 	it 'can edit user and change role' do
+		user = FactoryGirl.create(:user)
+		visit '/admin/user/1/edit'
 
+		within '#edit_user' do
+			fill_in 'Email',    with: 'replaceemail@test.com'
+      fill_in 'Password', with: 'fdsafdsa'
+      fill_in 'Password confirmation', with: 'fdsafdsa'
+      fill_in 'Roles', with: 'editor'
+      click_button('Save')
+		end
+
+    expect(current_path).to eq '/admin/user'
+    expect(page).to have_content("User successfully updated")
+    expect(page).to have_content("replaceemail@test.com")
+    expect(User.first.email).to eq("replaceemail@test.com")
+
+	end
+
+	it 'can delete a user' do
+		user = FactoryGirl.create(:user)
+		visit '/admin/user/1/delete'
+		click_button("Yes, I'm sure")
+
+    expect(current_path).to eq '/admin/user'
+    expect(page).to have_content("User successfully deleted")
 	end
 end
